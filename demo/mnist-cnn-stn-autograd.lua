bc6056e2bc239ab628fefa4e2dac6bfd0359dd59
Alex Wiltschko <alex.bw@gmail.com>
2015-12-01 19:50:48 -0500

    Working CNN, although tweaking required

--- demo/mnist-cnn-stn-autograd.lua
+++ demo/mnist-cnn-stn-autograd.lua
@@ -9,8 +9,8 @@ local stn = require 'stn'
 
 -- MNIST dataset 
 ---------------------------------
-local batchSize = 128
-local train, validation = paths.dofile("./distort_mnist.lua")(true, true, false, batchSize) -- batch, normalize, distort
+local batchSize = 256
+local train, validation = paths.dofile("./distort_mnist.lua")(true, true, true, batchSize) -- batch, normalize, distort
 local imageHeight, imageWidth = train.data:size(3), train.data:size(4)
 
 -- Set up confusion matrix
@@ -20,57 +20,71 @@ local confusionMatrix = optim.ConfusionMatrix({0,1,2,3,4,5,6,7,8,9})
 -- Get the STN functions
 ---------------------------------
 local fns = grad.functionalize('stn')
-local gridGenerator = fns.AffineGridGeneratorBHWD(imageHeight, imageWidth) -- grid is same size as image
+gridHeight = imageHeight/2
+gridWidth = imageWidth/2
+local gridGenerator = fns.AffineGridGeneratorBHWD(imageHeight/2, imageWidth/2) -- grid is same size as image
 local bilinearSampler = fns.BilinearSamplerBHWD()
 
 
 -- Set up our models
 ---------------------------------
--- Network that predicts warping from an image
-local nnetLoc, nnetLocParams = grad.model.NeuralNetwork({
-   -- number of input features:
-   inputFeatures = imageHeight*imageWidth,
-   hiddenFeatures = {100,100,6},
+-- Network that predicts warping from an image (conv then linear)
+local nnetLocConv, nnetLocParamsConv = grad.model.SpatialNetwork({
+   -- number of input features (maps):
+   inputFeatures = 1, -- grayscale input
+   hiddenFeatures = {20,20},
+   poolings = {2,2},
+   activations = 'Sigmoid',
+   kernelSize = 5,
+   -- dropoutProbs = {.1, .1},
+})
+x,y,n = train:getBatch(1)
+out = nnetLocConv(nnetLocParamsConv, x:double())
+nOut = torch.numel(out)/batchSize
+
+local nnetLocLin, nnetLocParamsLin = grad.model.NeuralNetwork({
+   inputFeatures = nOut,
+   hiddenFeatures = {20,6},
+   classifier = true,
+})
+
+-- Network that predicts class from a warped image (just linear, for now)
+local nnet, nnetParams = grad.model.NeuralNetwork({
+   inputFeatures = gridHeight*gridWidth,
+   hiddenFeatures = {128,128,10},
    activations = 'ReLU',
    classifier = true,
 })
 
--- Network that predicts class from a warped image
-local nnet, nnetParams = grad.model.NeuralNetwork({
-   inputFeatures = imageHeight*imageWidth,
-   hiddenFeatures = {100,100,10},
-   activations = 'ReLU',
-   classifier = true,
-})
+print(nnetLocParamsConv)
 
 -- Initialize the weights (really need a utility function for this!)
 ---------------------------------
-for _,_params in pairs({nnetParams,nnetLocParams}) do
+for _,_params in pairs({nnetParams,nnetLocParamsConv,nnetLocParamsLin}) do
    for i,weights in ipairs(_params) do
       weights.W = weights.W:cuda()
       weights.b = weights.b:cuda()
-      n = weights.W:size(1)
-      weights.W:uniform(-1/math.sqrt(n),1/math.sqrt(n))
+      weights.W:normal(0,0.1)
       weights.b:fill(0)
    end
 end
 
 -- Make sure that the net defaults to outputting identity transform
-local affineBias = nnetLocParams[#nnetLocParams].b:fill(0)
+local affineBias = nnetLocParamsLin[#nnetLocParamsLin].b:fill(0)
 affineBias[1] = 1
 affineBias[5] = 1
-nnetLocParams[#nnetLocParams].W:fill(0)
+nnetLocParamsLin[#nnetLocParamsLin].W:fill(0)
 
 -- Define our loss function
 ---------------------------------
 local function f(inputs, bhwdImages, labels)
    -- Flatten the input for the MLP
    local input = torch.view(bhwdImages, 
-                     batchSize, 
-                     imageHeight*imageWidth)
+                     batchSize, 1, imageHeight, imageWidth)
 
    -- Calculate how we should warp the image
-   local warpPrediction = nnetLoc(inputs.nnetLocParams, input)
+   local convOut = nnetLocConv(inputs.nnetLocParamsConv, input)
+   local warpPrediction = nnetLocLin(inputs.nnetLocParamsLin, torch.view(convOut, batchSize, nOut))
 
    -- Get 2D affine matrices
    local affineTransforms = torch.view(warpPrediction, batchSize, 2, 3)
@@ -92,21 +106,27 @@ local function f(inputs, bhwdImages, labels)
 
    -- Calculate loss
    local loss = grad.loss.logMultinomialLoss(prediction, labels)
-
    return loss, prediction, resampledImages
 end
 
 
 -- Set up the params we'll feed into our function
-params = {nnetParams=nnetParams, nnetLocParams=nnetLocParams}
+params = {
+   nnetParams=nnetParams, 
+   nnetLocParamsLin=nnetLocParamsLin,
+   nnetLocParamsConv=nnetLocParamsConv
+}
 
 -- Take the gradient of the function
 local g = grad(f, {optimize = true})
 
 local lr = 1e-2
 local w1
+local nBatches = train:getNumBatches()
 for epoch=1,100 do
-   for i=1,train:getNumBatches() do
+   print("Epoch " .. epoch)
+   for i=1,nBatches do
+      xlua.progress(i,nBatches)
 
       -- Get images in BHWD format, labels in one-hot format:
       local data, labels, n = train:getBatch(i)
@@ -121,14 +141,18 @@ for epoch=1,100 do
          params.nnetParams[k].W = params.nnetParams[k].W - grads.nnetParams[k].W*lr/n
          params.nnetParams[k].b = params.nnetParams[k].b - grads.nnetParams[k].b*lr/n
       end
-      for k=1,#params.nnetLocParams do
-         params.nnetLocParams[k].W = params.nnetLocParams[k].W - grads.nnetLocParams[k].W*lr/n
-         params.nnetLocParams[k].b = params.nnetLocParams[k].b - grads.nnetLocParams[k].b*lr/n
+      for k=1,#params.nnetLocParamsConv do
+         params.nnetLocParamsConv[k].W = params.nnetLocParamsConv[k].W - grads.nnetLocParamsConv[k].W*lr/n/1e2
+         params.nnetLocParamsConv[k].b = params.nnetLocParamsConv[k].b - grads.nnetLocParamsConv[k].b*lr/n/1e2
+      end
+      for k=1,#params.nnetLocParamsLin do
+         params.nnetLocParamsLin[k].W = params.nnetLocParamsLin[k].W - grads.nnetLocParamsLin[k].W*lr/n/1e1
+         params.nnetLocParamsLin[k].b = params.nnetLocParamsLin[k].b - grads.nnetLocParamsLin[k].b*lr/n/1e1
       end
 
       -- Log performance:
       confusionMatrix:batchAdd(prediction, target)
-      if i == 1 then -- train:getNumBatches() then
+      if i % 10 == 0 then -- train:getNumBatches() then
       -- if i > 0 then
          print(confusionMatrix)
          confusionMatrix:zero()
